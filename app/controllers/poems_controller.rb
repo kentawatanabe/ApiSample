8d6cccafc44328cc51c5c53950d760bfbb68a548
Kenta Watanabe <soukaijapan@gmail.com>
2017-02-06 04:48:15 +0000

    add_show

---
+++ app/controllers/poems_controller.rb
@@ -0,0 +1,14 @@
+class PoemsController < ApplicationController
+  before_action :set_poem, only:[:show]
+  def index
+    @poems = Poem.all
+    render json: @poems
+  end
+
+  def show
+  end
+
+  private
+    def set_poem
+      @poem = Poem.find(params[:id])
+end
